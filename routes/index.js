0fff6706dd4411f30efb81863d159b080e6bc933
stevovenom <stephenokoth2002@gmail.com>
2024-12-10 15:43:43 +0300

    Addition of teh connect, disconnect and users/me endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import UserController from '../controllers/UserController.js';
 
 const router = Router();
 
 // Existing endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 // New endpoint
-router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/diconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 export default router;
