70d6a3e068b807a44e2a1ee76b81117c8b2990f5
stevovenom <stephenokoth2002@gmail.com>
2024-12-10 17:23:12 +0300

    task 4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const router = Router();
 
@@ -10,5 +11,8 @@ router.get('/stats', AppController.getStats);
 
 // New endpoint
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
